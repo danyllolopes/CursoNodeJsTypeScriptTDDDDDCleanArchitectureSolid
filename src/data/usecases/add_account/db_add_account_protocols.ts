ef27682849a3cca239ea2b56f32ead3eac932cd3
Danyllo Cesar Lopes Leles <danyllo.leles@matrixdobrasil.com.br>
2022-01-13 10:58:56 -0300

    chore: first commit

---
+++ src/data/usecases/add_account/db_add_account_protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../domain/usecases/add_account'
+export * from '../../../domain/models/account'
+export * from '../../protocols/encrypter'
+export * from '../../protocols/add_account_repository'
